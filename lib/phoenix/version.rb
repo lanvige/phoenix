9e115d16f4eaaf07ec074d3394252071c2a42c36
Lanvige Jiang <lanvige@gmail.com>
2011-12-13 00:29:39 +0800

    Upgrade Phoenix Engine version to 0.1.0.beta

--- lib/phoenix/version.rb
+++ lib/phoenix/version.rb
@@ -3,7 +3,6 @@ version = File.read(File.expand_path("../../../PHOENIX_VERSION", __FILE__)).stri
 module Phoenix
   def self.version
     # Why can't use version from file
-    #version
-    "0.0.2.beta"
+    "0.1.0.beta"
   end
 end
