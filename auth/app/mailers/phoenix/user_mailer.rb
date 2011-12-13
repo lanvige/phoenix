fd54f414484bea609c82c31c7e2975ff160b8514
Lanvige Jiang <lanvige@gmail.com>
2011-12-13 19:22:32 +0800

    update user mailer

--- auth/app/mailers/phoenix/user_mailer.rb
+++ auth/app/mailers/phoenix/user_mailer.rb
@@ -6,8 +6,8 @@ module Phoenix
     
     default :from => "phoenix@lanvige.com"
     
-    def confirmation_instructions(record)
-      devise_mail(record, :confirmation_instructions)
+    def confirmation_instructions(user)
+      devise_mail(user, :confirmation_instructions)
     end
     
     # UserMailer.welcome_instructions(@user).deliver
@@ -16,12 +16,12 @@ module Phoenix
       mail(:to => "#{user.name} <#{user.email}>", :subject => "Welcome to use Phoenix Engine")
     end
 
-    def reset_password_instructions(record)
-      devise_mail(record, :reset_password_instructions)
+    def reset_password_instructions(user)
+      devise_mail(user, :reset_password_instructions)
     end
 
-    def unlock_instructions(record)
-      devise_mail(record, :unlock_instructions)
+    def unlock_instructions(user)
+      devise_mail(user, :unlock_instructions)
     end
       
     #def confirmation_instructions_bak(user)
@@ -30,7 +30,7 @@ module Phoenix
     #  mail(:to => "#{user.name} <#{user.email}>", :subject => "Confirm your account...")
     #end
     
-    def reset_password_instructions(user)
+    def reset_password_instructions_bak(user)
       @user = user
       @edit_password_reset_url = phoenix.edit_user_password_url(:reset_password_token => user.reset_password_token)
       mail(:to => user.email, :subject => "zheke.me" + ' ' + I18n.t(:password_reset_instructions))
