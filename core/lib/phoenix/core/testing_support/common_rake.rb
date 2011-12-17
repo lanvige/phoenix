ebf79542cdc3304489e1f7d1b01fbfd9e73d72c8
Lanvige Jiang <lanvige@gmail.com>
2011-12-17 15:59:48 +0800

    add rake test generator

---
+++ core/lib/phoenix/core/testing_support/common_rake.rb
@@ -0,0 +1,19 @@
+desc "Generates a dummy app for testing"
+namespace :common do
+  task :test_app do
+    require "#{ENV['LIB_NAME']}"
+
+    Phoenix::DummyGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--database=#{ENV['DB_NAME']}", "--quiet"]
+    Phoenix::SiteGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--quiet"]
+
+    puts "Setting up dummy database..."
+    cmd = "bundle exec rake RAILS_ENV=test AUTO_ACCEPT=true"
+    if RUBY_PLATFORM =~ /mswin/ #windows
+      cmd += " >nul"
+    else
+      cmd += " >/dev/null"
+    end
+
+    system(cmd)
+  end
+end
---
+++ lib/generators/phoenix/install/templates / config / initializers/phoenix.rb
@@ -0,0 +1,9 @@
+# Used to configure Phoenix Engine Preferences
+# 
+# In order to override a default setting set: config.setting_name = 'new value'
+#
+phoenix.config do |config|
+  # Example:
+  # Uncomment to override the default site name.
+  # config.site_name = "Phoenix Engine Demo Site"
+end
