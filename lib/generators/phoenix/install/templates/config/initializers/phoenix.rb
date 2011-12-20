75b6c385fe918db33500d8114614e9e4ca429953
Lanvige Jiang <lanvige@gmail.com>
2011-12-20 22:13:02 +0800

    Remove old generator.

--- lib/generators/phoenix/install/templates/config/initializers/phoenix.rb
+++
@@ -1,9 +0,0 @@
-# Used to configure Phoenix Engine Preferences
-# 
-# In order to override a default setting set: config.setting_name = 'new value'
-#
-phoenix.config do |config|
-  # Example:
-  # Uncomment to override the default site name.
-  # config.site_name = "Phoenix Engine Demo Site"
-end
